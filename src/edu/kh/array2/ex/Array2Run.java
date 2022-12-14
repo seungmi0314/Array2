fc7398c1c0143e40af435ace427ff7b7d45c2f97
seungmi <bbsm314@gmail.com>
2022-12-14 14:13:21 +0900

    Array2 Frist commit

---
+++ src/edu/kh/array2/ex/Array2Run.java
@@ -0,0 +1,17 @@
+package edu.kh.array2.ex;
+
+public class Array2Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayExample2 arreEx = new ArrayExample2();
+		
+		//arreEx.ex1();
+		arreEx.ex2();
+		
+		
+
+	}
+
+}
